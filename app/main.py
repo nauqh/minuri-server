7e386ec966f7642d26dfffe5ad5ef8ead1040b3c
kyomou <chonngai@gmail.com>
2026-04-12 22:05:31 +1000

    added endpoint to return available suburbs for population

--- app/main.py
+++ app/main.py
@@ -41,7 +41,25 @@ def get_population(data: populationQuery):
 
 @app.get("/api/get-Suburb")
 def get_suburb():
+    '''
+    Possible suburbs 2026:
+    Carlton
+    City of Melbourne
+    Docklands
+    East Melbourne
+    Kensington
+    Melbourne (CBD)
+    Melbourne (Remainder)
+    North Melbourne
+    Parkville
+    Port Melbourne
+    South Yarra
+    Southbank
+    West Melbourne (Industrial)
+    West Melbourne (Residential)
+    '''
     try:
-        pass
+        suburbs = ["Carlton", "City of Melbourne", "Docklands", "East Melbourne", "Kensington", "Melbourne (CBD)", "Melbourne (Remainder)", "North Melbourne", "Parkville", "Port Melbourne", "South Yarra", "Southbank", "West Melbourne (Industrial)", "West Melbourne (Residential)"]
+        return {"suburbs": suburbs}
     except Exception as e:
         return {"error": str(e)}
